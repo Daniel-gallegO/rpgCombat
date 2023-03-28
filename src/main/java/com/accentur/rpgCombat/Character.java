80b65d1027dad22e341b2fb1a53e5c7b26943046
Daniel-gallegO <danielgallegolopez@outlook.com>
2023-03-28 15:17:44 +0200

    iteration 1 add health test

---
+++ src/main/java/com/accentur/rpgCombat/Character.java
@@ -0,0 +1,2 @@
+package com.accentur.rpgCombat;public class Character {
+}
---
+++ src/test/java/com/accentur/rpgCombat/CharacterTest.java
@@ -0,0 +1,11 @@
+package com.accentur.rpgCombat;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CharacterTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
